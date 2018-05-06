5266c8af61f411812233cadd109b5e96a77dde7d
Ignasi Fosch <natx@y10k.ws>
2018-05-06 10:10:14 +0200

    Add simple pomodoro plugin

---
+++ reventlov/plugins/pomodoro/__init__.py
@@ -0,0 +1,54 @@
+import logging
+
+from telegram.ext import CommandHandler
+
+logger = logging.getLogger(__name__)
+
+class Bot(object):
+    def __init__(self, dispatcher):
+        dispatcher.add_handler(CommandHandler(
+            'set',
+            self.set_timer,
+            pass_args=True,
+            pass_job_queue=True,
+            pass_chat_data=True,
+        ))
+        dispatcher.add_handler(CommandHandler(
+            'unset',
+            self.unset_timer,
+            pass_chat_data=True,
+        ))
+        self.version = '0.0.1'
+        logger.info("Pomodoro plugin v{} loaded".format(self.version))
+
+    def alarm(self, bot, job):
+        bot.send_message(job.context['chat_id'], text=job.context['text'])
+
+    def set_timer(self, bot, update, args, job_queue, chat_data):
+        chat_id = update.message.chat_id
+        try:
+            due = int(args[0])
+            if due < 0:
+                update.message.reply_text(
+                    'Sorry we can not go back to future!'
+                )
+                return
+            job = job_queue.run_once(self.alarm, due, context={
+                'chat_id': chat_id,
+                'text': ' '.join(args[1:]) or 'Beep!'
+            })
+            chat_data['job'] = job
+            update.message.reply_text('Timer successfully set!')
+        except (IndexError, ValueError):
+            update.message.reply_text('Usage: /set <seconds> <message...>')
+
+    def unset_timer(self, bot, update, chat_data):
+        if 'job' not in chat_data:
+            update.message.reply_text('You have no active timer')
+            return
+
+        job = chat_data['job']
+        job.schedule_removal()
+        del chat_data['job']
+
+        update.message.reply_text('Timer successfully unset!')
