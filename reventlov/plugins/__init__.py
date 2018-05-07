befe7a768cff66c74c52eb8e473c2c5c51f50f30
Ignasi Fosch <natx@y10k.ws>
2018-05-07 23:26:50 +0200

    Add `/enable_plugin` command
    This commands allows user to load and enable plugins dynamically.
    It requires a disabled plugin name.

--- reventlov/plugins/__init__.py
+++ reventlov/plugins/__init__.py
@@ -6,6 +6,24 @@ import pkgutil
 logger = logging.getLogger(__name__)
 
 
+def get_disabled_plugins():
+    disabled_plugins = os.getenv('REVENTLOV_DISABLED_PLUGINS')
+    if disabled_plugins is None:
+        return []
+    return disabled_plugins.split(',')
+
+
+def add_plugin(plugin, dispatcher):
+    disabled_plugins = get_disabled_plugins()
+    os.environ['REVENTLOV_DISABLED_PLUGINS'] = ','.join([
+        plugin_name
+        for plugin_name in disabled_plugins
+        if plugin != plugin_name
+    ])
+    logger.info(f'Enabling plugin {plugin}: {disabled_plugins}')
+    return get_plugins(dispatcher)
+
+
 def iter_namespaces():
     return pkgutil.iter_modules(
         [os.path.dirname(__file__)],
@@ -14,7 +32,7 @@ def iter_namespaces():
 
 
 def find_plugins():
-    excluded = (os.getenv('REVENTLOV_DISABLED_PLUGINS') or '').split(',')
+    excluded = get_disabled_plugins()
     plugins = {
         name.split('.')[-1]: importlib.import_module(name)
         for finder, name, ispkg
